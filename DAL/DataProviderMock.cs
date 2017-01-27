0f18e8196ee4bf75c11c38c561043b715fdff713
colinhughes98 <colinhughes98@gmail.com>
2017-01-27 22:27:43 +0000

    add new data mock provider

---
+++ DAL/DataProviderMock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace DAL
+{
+    public class DataProviderMock : IDataProvider
+    {
+        public Task<bool> Update()
+        {
+           return new Task<bool>(()=> true);
+        }
+
+        public Task<MarketDetails> GetDataFromDB(int id)
+        {
+            return new Task<MarketDetails>(() => new MarketDetails() {BookiesOdds = 2, ID = 1, Market = "Asian Handicap"});
+        }
+    }
+}
